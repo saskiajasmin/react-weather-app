0c39339374bea6f982275e3ad8422ab0e351c420
saskiajasmin <122574944+saskiajasmin@users.noreply.github.com>
2023-02-23 19:23:19 +0100

    added github rep link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
       <h1>Weather App React</h1>
       <WeatherSearch />
       <footer className="Footer">
-        Open-Source Code <strong>@GitHub</strong>
+        Open-Source Code{" "}
+        <strong>
+          <a
+            href="https://github.com/saskiajasmin/react-weather-app"
+            target="_blank"
+            rel="noreferrer">
+            @GitHub
+          </a>
+        </strong>
       </footer>
     </div>
   );
